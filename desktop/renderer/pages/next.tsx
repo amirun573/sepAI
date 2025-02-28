6c9be8d50e0c23a2f83f0c626d4a28ecc9f1f39d
MOHD AMIRUN AQMAR BIN ROSLAN <amirunaqmar@gmail.com>
2025-03-01 00:57:11 +0800

    update create desktop

---
+++ desktop/renderer/pages/next.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Head from 'next/head'
+import Link from 'next/link'
+
+export default function NextPage() {
+  return (
+    <React.Fragment>
+    </React.Fragment>
+  )
+}
